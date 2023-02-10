b50ba43126dac22111ebf7ab4888778cf2aa7625
Arash Attari <attariarash@gmail.com>
2023-02-10 12:27:05 +0330

    documentation added

--- post/urls.py
+++ post/urls.py
@@ -4,3 +4,10 @@ urlpatterns = [
 path('<int:pk>/', PostDetail.as_view()),
 path('', PostList.as_view()),
 ]
+"""
+from rest_framework.routers import SimpleRouter                                
+router = SimpleRouter()                                                        
+router.register('users')                                                       
+router.register('')                                                             
+                      
+"""
--- todo_project/urls.py
+++ todo_project/urls.py
@@ -1,8 +1,17 @@
+from rest_framework.schemas import get_schema_view
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/',include('post.urls')),
     path('api-auth/', include('rest_framework.urls')), # new
+    path('api/usr/', include('UserList.urls')), # new
+     path('swagger-ui/',get_schema_view(
+            title="Your Project",
+            description="API for all things …",
+            version="1.0.0"
+        ), name='openapi-schema'),
+
 ]
